38a27505d92b61fcb4635e68418f0dc4878d8f91
Salvador Murillo <salvador.murillo4717@alumnos.udg.mx>
2021-03-05 21:28:18 +0000

    Creacion de Script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+clear
+
+numeroPro=0
+
+echo $'\t\t\t'"Estructura de Datos"
+echo $'\n'"Seleccione programa a ejecutar:"$'\n'
+
+NOMBRES="
+0.test 
+1.marcos 
+2.diego"
+
+for i in $NOMBRES;
+do
+  echo $i
+done
+
