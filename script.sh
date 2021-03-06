398c76b717e0e7ddbcd4f089e9f62634cfc67a98
salvador-murillo <murilloariass@gmail.com>
2021-03-06 08:28:44 -0600

    Verificacion en vscode

--- script.sh
+++ script.sh
@@ -6,28 +6,20 @@ numeroPro=0
 echo $'\t\t\t'"Estructura de Datos"
 echo $'\n'"Seleccione programa a ejecutar:"$'\n'
 
-# PROGRAMAS=(
-#     '00.Repaso - Manejo de Arreglos' 
-#     '01.Repaso - Funciones' 
-#     '3.test' 
-#     '4.jar')
-
-PROGRAMAS2=(
+PROGRAMAS=(
     '1.Repaso-Manejo-Arreglos' 
     '2.Repaso-Funciones' 
     '3.test')
 
-for i in ${PROGRAMAS2[@]};
+for i in ${PROGRAMAS[@]};
 do
     echo "$i"
 done
 
 read -p $'\n''Elige numero de programa: ' numeroPro
-cd "${PROGRAMAS2[$numeroPro-1]}"
+cd "${PROGRAMAS[$numeroPro-1]}"
 
 #make for macOS and clang for Repl.it
 clang++-7 -pthread -std=c++17 -o main main.cpp || make main && ./main
-
-#|| make main 
 
 
