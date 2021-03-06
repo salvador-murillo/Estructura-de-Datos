7950ecc0232d8b04c573e5550e771e72d41a504b
salvador-murillo <murilloariass@gmail.com>
2021-03-06 12:39:21 -0600

    Cambios en Sript para volver a seleccion de programas

--- script.sh
+++ script.sh
@@ -1,22 +1,33 @@
 #! /bin/bash
-clear
-
-numeroPro=0
-
-echo $'\t\t\t'"Estructura de Datos"
-echo $'\n'"Seleccione programa a ejecutar:"$'\n'
-
-PROGRAMAS=(
-    '1.Repaso-Manejo-Arreglos' 
-    '2.Repaso-Funciones' 
-    '3.test')
-
-for i in ${PROGRAMAS[@]};
+salir=1
+while true
 do
-    echo "$i"
+    clear
+    echo $'\t\t\t'"Estructura de Datos"
+    echo $'\n'"Seleccione programa a ejecutar:"$'\n'
+
+    PROGRAMAS=(
+        '1.Repaso-Manejo-Arreglos' 
+        '2.Repaso-Funciones' 
+        '3.test')
+
+    for i in ${PROGRAMAS[@]};
+    do
+        echo "$i"
+    done
+
+    numeroPro=0
+    read -p $'\n''Elige numero de programa: ' numeroPro
+
+    cd "${PROGRAMAS[$numeroPro-1]}"
+    pwd
+    clang++ -pthread -std=c++17 -o main main.cpp || make main && ./main
+
+    # clear
+    read -p $'Volver a seleccion de programas? 1-SI 2-NO : ' salir
+    if (($salir > 1))
+        then
+            exit
+    fi
+    cd ..
 done
-
-read -p $'\n''Elige numero de programa: ' numeroPro
-cd "${PROGRAMAS[$numeroPro-1]}"
-
-clang++ -pthread -std=c++17 -o main main.cpp || make main && ./main
